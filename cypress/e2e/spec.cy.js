29f47a74ca81565b4098cd4443b042168dc2d9e6
Rachel <Raquel.rudermano@gmail.com>
2022-08-18 10:48:46 -0700

    add repro

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,16 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  it('works without json params', () => {
+    cy.intercept('https://google.com*').as('google')
+    cy.visit('https://google.com')
+    cy.wait('@google')
+
+  })
+
+  // this test fails
+  it('works with json params', () => {
+    cy.intercept('https://google.com*').as('google')
+    cy.visit('https://google.com?json={"key":"value"}')
+    cy.wait('@google')
   })
 })
